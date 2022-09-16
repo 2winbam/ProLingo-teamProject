c1408918a8767a52ffd11bfdf2861be11aeeb651
Luyin6840 <fate2331@gmail.com>
2022-09-16 14:48:06 +0900

    shortcut icon변경 및 회원가입 페이지 추가 및 로그인 창 수정

--- src/main/java/net/softsociety/testboot/controller/MemberController.java
+++ src/main/java/net/softsociety/testboot/controller/MemberController.java
@@ -11,9 +11,17 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("member")
 public class MemberController {
 	
+	//로그인 화면
 	@GetMapping("/sign_in")
 	public String sign_in() {
 		log.debug("called sign_in");
 		return "sign_in";
+	}
+	
+	//회원가입 화면
+	@GetMapping("/sign_up")
+	public String sign_up() {
+		log.debug("called sign_up");
+		return "sign_up";
 	}
 }
