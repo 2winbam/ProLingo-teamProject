cd355dc663875032c3f4e10f0295c7c12830622b
Jungyeon <112139198+Jungyeon-Won@users.noreply.github.com>
2022-09-28 18:18:18 +0900

    개념 출력위한 컴파일러 폼 each문 작성, introdution 목록보여주는 each문을 잠시 주석처리함

--- src/main/resources/static/js/compiler/contents.js
+++ src/main/resources/static/js/compiler/contents.js
@@ -1,23 +1,21 @@
 /**
- * 
+ * 개념 슬라이드를 불러오는 스크립트문
  */
  
-  $(document).ready(function() {
-		
+ $(document).ready(function() {
+
 		contents();
 	});
 	
 	function contents(){
 		
-		console.log('개념 출력');
-			
 		$.ajax({
-			url:'contentsList',
+			url:'../contentsList',
 			type:'get',
 			dataType:'json',
 			success: output,
 			error: function(e){
-				console.log(JSON.stringify(e));	
+				console.log(JSON.stringify(e));
 			}
 		});
 	}
@@ -27,55 +25,22 @@
 		console.log(ob);
 		
 		let s = '';
-		/*
-		$.each(ob, function(index ,item){
-			s += '<div class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">';
-			s += '<img th:src=' + item.A_icon + 'alt="twbs" width="50" height="50" class="rounded-circle flex-shrink-0">';
-			s += '<div class="d-flex gap-2 w-100 justify-content-between">';
-			s += '<div><h6 class="mb-0">' + item.A_name + '</h6>';
-			s += '<p class="mb-0 opacity-75">' + item.A_text + '</p></div>';
-			s += '<small class="opacity-50 text-nowrap">' + item.A_date + '</small></div></div>';
-		});
-		$('#output').html(s); 
-		
+
 		$.each(ob, function(index ,item){
 			s += '<div class="modal-lesson-content">';
 			s += '<div class="modal-lesson-header d-flex justify-content-end">';
-			s += '<a class="contentClose">';
-			s += '<span aria-hidden="true"><i class="bi bi-x-circle-fill"></i></span>';
-			s += '</a></div><div class="modal-lesson-body">';
-			s += '<h2>'+ item.LESSON_SUBJECT +'</h2>';	
-			s += '<div class="modal-lesson-body-content">';
-			s += '<p class="slide-top__description"><span><font style="vertical-align: inherit;">';
-			s += '<font style="vertical-align: inherit;">' + item.LESSON_CONTENTS + '</font></font></span><br>' 
-							<span><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">처음 듣는 단어도 많이 어렵다고 느낄 수도 있을지도 모릅니다만, </font></font></span><br> <span><font
-								style="vertical-align: inherit;"><font
-									style="vertical-align: inherit;">하나하나 순서를 쫓아 배워 가므로, 끝까지
-										노력해 갑시다.</font></font></span><br> <span></span>
-						</p>
-					
-					</div>
-						<div class="slide__SlideBottom-z90lcr-1 wNtlP">
-							<div
-								class="slide_bottom_items__SlideBottomItems-sc-1g258za-0 cVxUeS">
-								<div
-									class="slide_bottom_items__SlideBottomItemsInnerWrapper-sc-1g258za-1 eIFOJW">
-									<div 
-										class="slide_bottom_items__SlideBottomItem-sc-1g258za-2 kDyeWH">
-										<div class="u-inline">
-											<div class="u-relative">
-												<img data-id="1377"
-													class="slide_image1"
-													src="https://d2aj9sy12tbpym.cloudfront.net/progate/shared/images/slide/java/study/1/1464330632364.png">
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-				</div>
-			</div>
-		*/
-		
+			s += '<a class="contentClose"><span aria-hidden="true"><i class="bi bi-x-circle-fill"></i></span></a></div>';
+			s += '<div class="modal-lesson-body">';
+			s += '<div class="d-flex flex-row justify-content-center">';
+			s += '<div class="contents modal-lesson-body-title"><h3>' + item.lesson_subtitle + '</h3></div>';
+			s += '<div class="contents modal-lesson-body-content">';
+			s += '<p class="slide-top__description"><span>' + item.lesson_contents + '</span></p></div></div>';
+			s += '<div class="slide__SlideBottom-z90lcr-1 wNtlP">';
+			s += '<div class="slide_bottom_items__SlideBottomItems-sc-1g258za-0 cVxUeS">';
+			s += '<div class="slide_bottom_items__SlideBottomItemsInnerWrapper-sc-1g258za-1 eIFOJW">';
+			s += '<div class="slide_bottom_items__SlideBottomItem-sc-1g258za-2 kDyeWH">';
+			s += '<div class="u-inline"><div class="u-relative"><img class="slide_image1" th:src="' + item.lesson_picture +'"></div></div></div></div></div></div></div></div>';
+		});
+		$('#slider').html(s);
 	}
 		
