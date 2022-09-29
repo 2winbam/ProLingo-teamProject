d820ebddade949e5850a27e8616d3a1ceb78fc26
su0508kr <su0508kr@gmail.com>
2022-09-29 10:42:14 +0900

    정답 맞추면 경험치 올리는 기능 추가, 유저 vo 경험치 변수명 변경

--- src/main/java/net/softsociety/testboot/controller/CompilerController.java
+++ src/main/java/net/softsociety/testboot/controller/CompilerController.java
@@ -7,12 +7,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.extern.slf4j.Slf4j;
+import net.softsociety.testboot.domain.MemberVO;
 import net.softsociety.testboot.service.DBTestService;
+import net.softsociety.testboot.service.MemberService;
 
 @Controller
 @Slf4j
@@ -23,6 +27,9 @@ public class CompilerController {
 	
 	@Autowired
 	DBTestService dbts;
+	
+	@Autowired
+	MemberService ms;
 
 	@PostMapping("compile")
 	@ResponseBody
@@ -137,5 +144,41 @@ public class CompilerController {
 			e.printStackTrace();
 		}
 		return null;
+	}
+	
+	/**
+	 * 
+	 * @param lessonid 지금은 question 아이디만 알면 필요 없지만 나중에 lessonid, index 복합키로 바꾼다면 필요
+	 * @param questid
+	 * @return
+	 */
+	@PostMapping("lessoncomplite")
+	@ResponseBody
+	public String lessoncomplite(int lesson_id, int question_id, @AuthenticationPrincipal UserDetails user) {
+		log.debug("레슨 : {} 퀘스쳔 : {}", lesson_id, question_id);
+		
+		//로그인 한 유저가 있으면
+		if(user != null) {
+			//lesson의 question을 이미 완료했는지 확인하는 코드 필요
+			
+			//완료하지 않은 문제였다면
+			//그 유저의 경험치를 10만큼 추가
+			int result = ms.updateUserExp(user.getUsername(), 10);
+			//성공시
+			if(result != 0) {
+				//경험치를 확인하기 위해
+				MemberVO member = ms.getMemerInfo(user.getUsername());
+				
+				return "user " + member.getUser_name() + "의 경험치 : " + member.getUser_exp();
+			}
+			else {
+				return "lessoncomplite fail";
+			}
+			
+			//완료한 문제였다면
+			//return "이미 경험치 획득한 문제";
+		}
+		
+		return "로그인 한 유저 없음";
 	}
 }
