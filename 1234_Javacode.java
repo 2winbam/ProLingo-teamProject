cdb77e27d1c4e2f8e12fed97af266f0142476745
tmdqja92 <110378945+tmdqja92@users.noreply.github.com>
2022-09-29 11:48:36 +0900

    프로필 수정중

--- 1234_Javacode.java
+++ 1234_Javacode.java
@@ -1,9 +1,7 @@
-class Main{
+class Main {
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("Hello Java");
-=======
-		System.out.println("Hello World!");
->>>>>>> ddcd754c8b0df3d517124692184f52ca982be715
+
+		This line should be a comment
+
 	}
 }
