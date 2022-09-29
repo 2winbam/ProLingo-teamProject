88ce768e775614818919179fe184a42ffeb87efd
tmdqja92 <110378945+tmdqja92@users.noreply.github.com>
2022-09-29 12:45:19 +0900

    no message

--- 1234_Javacode.java
+++ 1234_Javacode.java
@@ -1,7 +1,9 @@
 class Main {
 	public static void main(String[] args) {
 
-		This line should be a comment
+		//This line should be a comment
+      
+     	System.out.println("Hellow Java");
 
 	}
 }
