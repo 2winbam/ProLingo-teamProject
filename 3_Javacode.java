eea9ac59026ab1e3b1c3a400dfdf847aca19718d
tmdqja92 <110378945+tmdqja92@users.noreply.github.com>
2022-10-05 19:55:57 +0900

    no message

--- 3_Javacode.java
+++ 3_Javacode.java
@@ -1,5 +1,12 @@
-class Main{
-	public static void main(String[] args) {
-		System.out.println("Hello Java");
+import java.time.LocalTime;class Main {public static void main(String[] args) {int st = LocalTime.now().getNano();Usercode.usercode();int et = LocalTime.now().getNano();System.out.println("forsplit" + (et - st) / (double)1000000000);}}class Usercode {static void usercode() {
+		// Print the result of 12 divided by 3
+		System.out.println(4);
+
+		// Print the result of 3 times 6
+		System.out.println(3 * 6);
+
+		// Print the remainder of 8 %divided by 3
+		System.out.println(2);
+
 	}
 }
