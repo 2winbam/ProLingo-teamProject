bed01f34c6a7eedcece5b5770b2b7be17cf29046
Luyin6840 <fate2331@gmail.com>
2022-10-05 22:22:40 +0900

    게시판 수정 및 댓글 구현중

--- src/main/java/net/softsociety/testboot/controller/BoardController.java
+++ src/main/java/net/softsociety/testboot/controller/BoardController.java
@@ -20,6 +20,8 @@ import net.softsociety.testboot.dao.BoardDAO;
 import net.softsociety.testboot.domain.BoardVO;
 import net.softsociety.testboot.domain.BoardWithName;
 import net.softsociety.testboot.domain.PageNavigator;
+import net.softsociety.testboot.domain.ReplyVO;
+import net.softsociety.testboot.domain.ReplyWithName;
 import net.softsociety.testboot.service.BoardService;
 import net.softsociety.testboot.service.MemberService;
 
@@ -68,13 +70,17 @@ public class BoardController {
 		//ArrayList<BoardVO> boardList = service.boardListAll(navi,type,searchWord);
 		ArrayList<BoardWithName> boardList = service.boardListAll(navi,type,searchWord);
 		
-		
+		//인기글 리스트
+		ArrayList<BoardWithName> hitList = service.hitListAll(navi,type,searchWord);
+			
 		//ArrayList<Board> 타입으로 모델에 저장
 		model.addAttribute("navi", navi);
 		model.addAttribute("type", type);
 		model.addAttribute("boardlist", boardList);
 		model.addAttribute("searchWord", searchWord);
 		model.addAttribute("mode", mode);
+		model.addAttribute("hitList", hitList);
+		
 		
 		return "boardView/boardList";
 	}
@@ -131,14 +137,70 @@ public class BoardController {
 		//조회수 증가
 		service.updateHits(board_id);
 		
-		//현재 글의 리플 목록 읽기
-
+		//해당 글의 댓글 목록 읽기
+		ArrayList<ReplyWithName> replylist = service.replyList(board_id);
 		
 		//결과를 모델에 담아서 html에 출력
 		model.addAttribute("writingRead", board);
-		//model.addAttribute("replylist", replylist);
+		model.addAttribute("replylist", replylist);
 			
 		return "boardView/read";
 	}
 	
+	/**
+	 * 글 삭제
+	 */
+	@GetMapping("delete")
+	public String delete(
+			int board_id
+			, @AuthenticationPrincipal UserDetails user
+			) {
+		//해당번호의 글 조회
+		BoardVO board = service.boardNum(board_id);
+		
+		//결과가 없으면 글 목록으로 다이렉트
+		if (board == null) {
+			return "direct: list";
+		}
+		//로그인한 아이디 확인
+		String id = user.getUsername();
+		int userID = Integer.parseInt(id);	
+		board.setUser_id(userID); 
+		
+		//전달된 번회의 글 삭제
+		log.debug("전달된 값 : {}", board);
+		
+		int result = service.deleteList(board);
+		
+		if(result == 0) {
+			log.debug("삭제 성공");
+		}
+		else {
+			log.debug("삭제 실패");
+		}
+		
+		return "redirect:list";
+	}
+	
+	/**
+	 * 댓글 저장
+	 */
+	@PostMapping("replyWrite")
+	public String replyWrite(
+			ReplyWithName reply
+			, @AuthenticationPrincipal UserDetails user
+			) {
+		//본문글번호, 리플내용 전달받아 로그인한 아이디 추가
+		reply.setNickname(user.getUsername());
+		
+		//DB에 저장
+		log.debug("replyWrite() called");
+		log.debug("저장할 댓글 : {}", reply);
+		
+		int result = service.replyWrite(reply);
+		
+		log.debug("결과 : {}", result);
+			
+		return "redirect:/board/read?board_id=" +reply.getBoard_id();
+	}
 }
---
+++ src/main/java/net/softsociety/testboot/domain/ReplyWithName.java
@@ -0,0 +1,15 @@
+package net.softsociety.testboot.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReplyWithName extends ReplyVO{
+	
+	private String nickname;
+	
+}
+
